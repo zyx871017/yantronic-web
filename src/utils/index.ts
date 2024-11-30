cbc4b7e4a9cf972a880c24ddc41170cb6623f6ea
ZhaoYuXiang <yuxiangz@ebtech.com>
2024-11-30 16:37:47 +0800

    fix window undefined problem

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ export const isLogin = () => {
 };
 
 export const isMobile = () => {
-  if (window) {
+  if (typeof window !== "undefined") {
     return window?.innerWidth < 768;
   }
   return false;
